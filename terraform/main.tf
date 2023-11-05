ef79474f82780b2f7b3a5fe4afbb4fe231174b3e
Chukwu Emmanuel <eechukwu@gmail.com>
2023-11-05 22:06:36 +0000

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,3 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "s3_bucket" {
-  source = "./modules/s3"
-
-  bucket_name = var.bucket_name
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,4 +7,5 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
+  default = "kenejdhsbns"
 }
