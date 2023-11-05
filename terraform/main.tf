b2019786a2f125833cb56f3b54ed55a0aa964d80
Chukwu Emmanuel <eechukwu@gmail.com>
2023-11-05 22:54:44 +0000

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "future2023bucket"   # Your S3 bucket name
+    key    = "state/terraform.tfstate" # Path within the bucket to store the state file
+    region = "eu-west-2"          # Replace with the region your bucket is in
+    # Additional configurations can be added if needed
+  }
+}
+
+# Your other Terraform resources...
+
